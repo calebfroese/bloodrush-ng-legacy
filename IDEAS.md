bf2ddc0c284079b95d37703373afe90392be1fbc
Caleb Froese <caleb.froese@gmail.com>
2017-01-02 13:05:15 +1030

    Add scrollable games viewer

---
+++ IDEAS.md
@@ -0,0 +1,10 @@
+# Ideas
+## Home vs Away Games
+* Above a certain team level, you can upload an image of your team's top-down arena
+* Live games (pre-calculate movements and everything, but send it out live)
+* Money
+- Able to purchase players from lists
+- Upgrade stadium
+-- Random stadium names
+-- Bigger venue = more capacity
+-- Fans to generate income
--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -2,9 +2,28 @@ import { Component } from '@angular/core';
 
 @Component({
     selector: 'app-home',
-    templateUrl: './home.component.html'
+    templateUrl: './home.component.html',
+    styleUrls: ['./home.component.scss']
 })
 export class HomeComponent {
     teamWins: number = 5;
     teamLosses: number = 8;
+    games: any = [
+        {
+            'team': 'Burnside Blazers',
+            'venue': 'MTS Center'
+        },
+        {
+            'team': 'Banksia Vikings',
+            'venue': 'All-Star Arena'
+        },
+        {
+            'team': 'Golden Grove Chasers',
+            'venue': 'GZ'
+        },
+        {
+            'team': 'Redwood Park Rangers',
+            'venue': 'Gumroad Stadium'
+        }
+    ];
 }
