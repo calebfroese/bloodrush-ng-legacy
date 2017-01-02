e0de06404cfeded2a660a69cff81b87c608a5d1d
Caleb Froese <caleb.froese@gmail.com>
2017-01-02 22:48:36 +1030

    Added to game detail page

--- src/app/season/game/game-detail.component.ts
+++ src/app/season/game/game-detail.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Params } from '@angular/router';
 import { Location } from '@angular/common';
+import * as moment from 'moment';
 
 import { MongoService } from './../../mongo/mongo.service';
 
