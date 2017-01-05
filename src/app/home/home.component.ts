1f1ec445d4523a6cea2ac03ca6808f56d14914e4
Caleb Froese <caleb.froese@gmail.com>
2017-01-05 22:37:00 +1030

    Add for undecided teams

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -17,23 +17,37 @@ export class HomeComponent implements OnInit {
     constructor(private mongo: MongoService, private router: Router) { }
 
     ngOnInit(): void {
+        this.mongo.fetchSeasonByActive().then(season => {
+            console.log(season)
+            this.season = season;
+            for (let i = 0; i < this.season.games.length; i++) {
+                if (this.season.games[i]['round'] === parseInt(this.season.games[i]['round'], 10)) {
+                    if (this.season.games[i]['home']) {
+                        this.mongo.fetchTeamById(this.season.games[i]['home']).then(teamHome => {
+                            // debugger;
+                            this.season.games[i]['home'] = teamHome;
+                        });
+                    } else {
+                        this.season.games[i]['home'] = { name: 'Bye' };
+                    }
+                    if (this.season.games[i]['away']) {
+                        this.mongo.fetchTeamById(this.season.games[i]['away']).then(teamAway => {
+                            this.season.games[i]['away'] = teamAway;
+
+                        });
+                    } else {
+                        this.season.games[i]['away'] = { name: 'Bye' };
+                    }
+                } else {
+                    this.season.games[i]['home'] = { name: 'Undecided' };
+                    this.season.games[i]['away'] = { name: 'Undecided' };
+                }
+            }
+        }).catch(err => {
+            debugger;
+        });
         this.mongo.fetchTeams().then(teamsArray => {
             this.teams = teamsArray;
-            this.mongo.fetchSeasonByActive().then(season => {
-                this.season = season;
-                for(let i = 0; i < this.season.games.length; i++) {
-                    this.mongo.fetchTeamById(this.season.games[i]['home']).then(teamHome => {
-                        // debugger;
-                        this.season.games[i]['home'] = teamHome;
-                    });
-                    this.mongo.fetchTeamById(this.season.games[i]['away']).then(teamAway => {
-                        this.season.games[i]['away'] = teamAway;
-                        console.log(teamAway)
-                    });
-                }
-            }).catch(err => {
-                debugger;
-            });
         }).catch(err => {
             debugger;
         });
