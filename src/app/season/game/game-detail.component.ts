377fa91ad67804f0dcb6295a50a0698a7558fa01
Caleb Froese <caleb.froese@gmail.com>
2017-01-08 14:44:34 +1030

    Add background

--- src/app/season/game/game-detail.component.ts
+++ src/app/season/game/game-detail.component.ts
@@ -7,24 +7,24 @@ import { MongoService } from './../../mongo/mongo.service';
 
 @Component({
     templateUrl: './game-detail.component.html',
-    styles: [`
-    canvas, img {
-        image-rendering: optimizeSpeed;
-        image-rendering: -moz-crisp-edges;
-        image-rendering: -webkit-optimize-contrast;
-        image-rendering: optimize-contrast;
-        image-rendering: pixelated;
-        -ms-interpolation-mode: nearest-neighbor;
-        border: 5px solid #ddd;
-    }
-`]
+    //     styles: [`
+    //     canvas, img {
+    //         image-rendering: optimizeSpeed;
+    //         image-rendering: -moz-crisp-edges;
+    //         image-rendering: -webkit-optimize-contrast;
+    //         image-rendering: optimize-contrast;
+    //         image-rendering: pixelated;
+    //         -ms-interpolation-mode: nearest-neighbor;
+    //         border: 5px solid #ddd;
+    //     }
+    // `]
 })
 export class GameDetailComponent implements OnInit {
     isBye: boolean = false;
     nonByeTeam: string; // index of the team that is not null
     game: any;
     gameId: number;
-    season: any;    
+    season: any;
 
     @ViewChild('gameCanvas') gameCanvas: ElementRef;
     @ViewChild('cDiv') cDiv: ElementRef;
@@ -35,6 +35,8 @@ export class GameDetailComponent implements OnInit {
     fps = 30;
     // Scaling
     ratio: number;
+    maxWidth: number = 960;
+    maxHeight: number = 600;
 
     constructor(
         private route: ActivatedRoute,
@@ -81,7 +83,7 @@ export class GameDetailComponent implements OnInit {
 
     // CANVAS
     playGame(game: any): void {
-        this.loadImage("char");
+        this.loadImage('char');
 
         this.initCanvas();
         this.fullscreenify();
@@ -92,7 +94,7 @@ export class GameDetailComponent implements OnInit {
         this.images[name].onload = () => {
             this.redrawCanvas();
         };
-        this.images[name].src = "/assets/" + name + ".jpg";
+        this.images[name].src = '/assets/' + name + '.png';
     }
 
     initCanvas(): void {
@@ -105,7 +107,8 @@ export class GameDetailComponent implements OnInit {
         let canvasHeight = this.gameCanvas.nativeElement.height;
 
         this.context.clearRect(0, 0, this.gameCanvas.nativeElement.width, this.gameCanvas.nativeElement.height);
-        this.context.drawImage(this.images["char"], this.x, 200, this.images['char'].width / this.ratio, this.images['char'].height / this.ratio);
+        this.drawField();
+        this.context.drawImage(this.images['char'], this.x / this.ratio, this.y / this.ratio, this.images['char'].width / this.ratio, this.images['char'].height / this.ratio);
         setTimeout(() => {
             window.setTimeout(this.redrawCanvas(), 1000 / this.fps);
         }, 30);
@@ -124,6 +127,14 @@ export class GameDetailComponent implements OnInit {
         this.gameCanvas.nativeElement.width = containerWidth;
         this.gameCanvas.nativeElement.height = containerWidth / 1.6;
 
-        this.ratio = 969.09 / containerWidth;
+        this.ratio = this.maxWidth / containerWidth;
+    }
+
+    drawField(): void {
+        // Draws the field
+        this.context.beginPath();
+        this.context.rect(0, 0, this.maxWidth, this.maxHeight);
+        this.context.fillStyle = 'green';
+        this.context.fill();
     }
 }
