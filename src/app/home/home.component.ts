155b2921300d644e253de6e51ff47b5a3c4d0049
Caleb Froese <caleb.froese@gmail.com>
2017-01-14 00:19:09 +1030

    Load players from logged in account

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -14,38 +14,40 @@ export class HomeComponent implements OnInit {
     teamWins: number = 5;
     teamLosses: number = 8;
     teams: any; // array of teams in the league sorted by points
+    myTeam: any;
 
     constructor(private mongo: MongoService, private router: Router, private acc: AccountService) { }
 
     ngOnInit(): void {
+        this.loadMyTeam();
         this.mongo.run('seasons', 'allActive', {})
-        .then(seasons => {
-            let season = seasons[0];
-            this.season = season;
-            for (let i = 0; i < this.season.games.length; i++) {
-                if (this.season.games[i]['round'] === parseInt(this.season.games[i]['round'], 10)) {
-                    if (this.season.games[i]['home']) {
-                        this.mongo.run('teams', 'oneById', { _id: this.season.games[i]['home'] }).then(teamHome => {
-                            this.season.games[i]['home'] = teamHome;
-                        });
-                    } else {
-                        this.season.games[i]['home'] = { name: 'Bye' };
-                    }
-                    if (this.season.games[i]['away']) {
-                        this.mongo.run('teams', 'oneById', { _id: this.season.games[i]['away'] }).then(teamAway => {
-                            this.season.games[i]['away'] = teamAway;
+            .then(seasons => {
+                let season = seasons[0];
+                this.season = season;
+                for (let i = 0; i < this.season.games.length; i++) {
+                    if (this.season.games[i]['round'] === parseInt(this.season.games[i]['round'], 10)) {
+                        if (this.season.games[i]['home']) {
+                            this.mongo.run('teams', 'oneById', { _id: this.season.games[i]['home'] }).then(teamHome => {
+                                this.season.games[i]['home'] = teamHome;
+                            });
+                        } else {
+                            this.season.games[i]['home'] = { name: 'Bye' };
+                        }
+                        if (this.season.games[i]['away']) {
+                            this.mongo.run('teams', 'oneById', { _id: this.season.games[i]['away'] }).then(teamAway => {
+                                this.season.games[i]['away'] = teamAway;
 
-                        });
+                            });
+                        } else {
+                            this.season.games[i]['away'] = { name: 'Bye' };
+                        }
                     } else {
-                        this.season.games[i]['away'] = { name: 'Bye' };
+                        // Playoffs, leave names as are
                     }
-                } else {
-                    // Playoffs, leave names as are
                 }
-            }
-        }).catch(err => {
-            debugger;
-        });
+            }).catch(err => {
+                debugger;
+            });
         this.mongo.run('teams', 'all', {}).then(teamsArray => {
             this.teams = teamsArray;
         }).catch(err => {
@@ -57,4 +59,19 @@ export class HomeComponent implements OnInit {
         this.router.navigate(['/season/' + this.season.number + '/' + gameId]);
     }
 
+    loadMyTeam(): void {
+        if (!this.acc.loggedInAccount._id) {
+            console.log('no team!');
+            return;
+        }
+
+        this.mongo.run('teams', 'oneByOwner', { ownerId: this.acc.loggedInAccount._id })
+            .then(myTeam => {
+                console.log(myTeam)
+                this.myTeam = myTeam;
+            })
+            .catch(err => {
+                debugger;
+            });
+    }
 }
