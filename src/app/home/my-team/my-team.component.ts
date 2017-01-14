b1e2ef0a817e7255c0dfe5564ada157c031bb07f
Caleb Froese <caleb.froese@gmail.com>
2017-01-15 09:17:09 +1030

    Add buttons to locally move players

--- src/app/home/my-team/my-team.component.ts
+++ src/app/home/my-team/my-team.component.ts
@@ -21,11 +21,32 @@ export class MyTeamComponent implements OnInit {
 
     loadTeam(): void {
         this.mongo.run('teams', 'oneByOwner', { ownerId: this.acc.loggedInAccount._id })
-        .then(team => {
-            this.team = team;
-        })
-        .catch(err => {
-            debugger
-        });
+            .then(team => {
+                this.team = team;
+            })
+            .catch(err => {
+                debugger
+            });
     }
+
+    moveUp(index): void {
+        // Moves a player up a space
+        var temp = this.team.players[index];
+        this.team.players[index]
+    }
+
+    moveDown(index): void {
+        // Moves a player down a space
+    }
+
+    moveTo(old_index, new_index) {
+        if (new_index >= this.team.players.length) {
+            let k = new_index - this.team.players.length;
+            while ((k--) + 1) {
+                this.team.players.push(undefined);
+            }
+        }
+        this.team.players.splice(new_index, 0, this.team.players.splice(old_index, 1)[0]);
+        // return this.team.players; // for testing purposes
+    };
 }
