9398296a499d9c942aa3402262ac333b8d9b0a0c
Caleb Froese <caleb.froese@gmail.com>
2017-01-29 22:59:51 +1030

    Show the team image on the team detail page

--- src/app/team/team-detail.component.ts
+++ src/app/team/team-detail.component.ts
@@ -2,15 +2,25 @@ import { Component, OnInit, NgZone } from '@angular/core';
 import { ActivatedRoute, Params } from '@angular/router';
 import { Location } from '@angular/common';
 
+import { Config } from './../shared/config';
+import { environment } from './../../environments/environment';
 import { AccountService } from './../shared/account.service';
 import { ApiService } from './../shared/api/api.service';
 
 @Component({
-    templateUrl: './team-detail.component.html'
+    templateUrl: './team-detail.component.html',
+    styles: [`
+        .team-icon {
+            width: 100px;
+            height: 100px;
+        }
+    `]
 })
 export class TeamDetailComponent implements OnInit {
     team: any;
     players: any;
+    config = Config;
+    envName = environment.envName;
 
     constructor(
         private route: ActivatedRoute,
