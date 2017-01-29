5e547504457b33fe65b4aefa211a4855f977646c
Caleb Froese <caleb.froese@gmail.com>
2017-01-29 16:51:20 +1030

    Add player orientation saving functionality

--- src/app/my/players/my-players.component.ts
+++ src/app/my/players/my-players.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 import { ApiService } from './../../shared/api/api.service';
 import { AccountService } from './../../shared/account.service';
@@ -6,16 +6,32 @@ import { AccountService } from './../../shared/account.service';
 @Component({
     templateUrl: './my-players.component.html'
 })
-export class MyPlayersComponent {
+export class MyPlayersComponent implements OnInit {
+
+    players: any[] = [];
 
     constructor(
         private acc: AccountService,
         private api: ApiService
-    ) {
-        this.players = this.acc.players;
-    }
+    ) { }
 
-    players: any[] = [];
+    ngOnInit(): void {
+        // Load the players
+        if (this.acc.team.playerIdsAtPos && this.acc.team.playerIdsAtPos.length > 0) {
+            console.log('Loading from pos');
+            console.log(this.acc.team.playerIdsAtPos);
+            for (let i = 0; i < this.acc.team.playerIdsAtPos.length; i++) {
+                this.acc.players.forEach(player => {
+                    if (player.id === this.acc.team.playerIdsAtPos[i]) {
+                        this.players[i] = player;
+                    }
+                });
+            }
+        } else {
+            console.log('Loading default');
+            this.players = this.acc.players;
+        }
+    }
 
     moveTo(old_index, new_index) {
         if (!this.players[new_index]) return;
@@ -31,15 +47,15 @@ export class MyPlayersComponent {
 
     saveTeam(): void {
         // Saves the team to the current listing of players
-        // TODO: Server-side protection of meddling
-        console.error('save team!');
-        debugger;
-        // this.mongo.run('teams', 'saveMyTeam', { team: this.acc.team })
-        //     .then(res => {
-        //         alert('Team saved successfully.');
-        //     })
-        //     .catch(err => {
-        //         debugger;
-        //     });
+        let playerIdsAtPos = [];
+        for (let i = 0; i < this.players.length; i++) {
+            playerIdsAtPos[i] = this.players[i].id;
+        }
+        this.api.run('patch', `/teams/${this.acc.team.id}`, '', {
+            playerIdsAtPos: playerIdsAtPos
+        })
+            .subscribe(team => {
+                console.log(team);
+            });
     }
 }
