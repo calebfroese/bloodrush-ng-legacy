22047ddb17c6a3a4792588a79b3021644349a734
Caleb Froese <caleb.froese@gmail.com>
2017-02-04 17:28:52 +1030

    Fetch the actual players from the database who are for sale (market)

--- src/app/market/market.component.ts
+++ src/app/market/market.component.ts
@@ -1,46 +1,33 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+
+import { ApiService } from './../shared/api/api.service';
+import { AccountService } from './../shared/account.service';
 
 @Component({
     templateUrl: './market.component.html'
 })
-export class MarketComponent {
-    mockPlayers = [
-        {
-            first: 'Bob',
-            last: 'Bobrovski',
-            price: 62,
-            atk: 52,
-            def: 61,
-            spd: 80,
-            kg: 90,
-            rec: 20,
-            state: 'market'
-        },
-        {
-            first: 'John',
-            last: 'Spencer',
-            price: 182,
-            atk: 52,
-            def: 61,
-            spd: 80,
-            kg: 90,
-            rec: 20,
-            state: 'market'
-        },
-        {
-            first: 'Henry',
-            last: 'Michaels',
-            price: 53,
-            atk: 52,
-            def: 61,
-            spd: 80,
-            kg: 90,
-            rec: 20,
-            state: 'market'
-        }
-    ];
-    players: any[] = this.mockPlayers;
+export class MarketComponent implements OnInit {
+    players: any[];
+    myPlayers: any[];
     modalPlayer: any; // the player being viewed in a modal
+
+    constructor(private api: ApiService, private acc: AccountService) { }
+
+    ngOnInit(): void {
+        // Load the players
+        this.api.run('get', `/players`, '&filter={"where": {"state": "market"}}', {})
+            .then(playersForSale => {
+                this.players = playersForSale;
+            }).catch(err => {
+                debugger;
+            });
+        this.api.run('get', `/teams/${this.acc.team.id}/players`, '&filter={"where": {"state": "market"}}', {})
+            .then(playersForSale => {
+                this.myPlayers = playersForSale;
+            }).catch(err => {
+                debugger;
+            });
+    }
 
     viewPlayer(player) {
         this.modalPlayer = player;
