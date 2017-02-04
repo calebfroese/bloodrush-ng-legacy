e89536d4b53107897cc7509c2c0b3a1794e795be
Caleb Froese <caleb.froese@gmail.com>
2017-02-04 16:59:47 +1030

    Fill market page with test data

--- src/app/market/market-listings.component.ts
+++ src/app/market/market-listings.component.ts
@@ -5,9 +5,16 @@ import * as moment from 'moment';
 @Component({
     selector: 'bloodrush-market-listings',
     template: 'bloodrush-market-listings',
-    templateUrl: './market-listings.component.html'
+    templateUrl: './market-listings.component.html',
+    styles: [`
+        .listing-button {
+            float: right;
+            margin-left: 5px;
+        }
+    `]
 })
 export class MarketListingsComponent {
     @Input() players: any[];
+    @Input() isMine: boolean;
     mockTime: string = moment().format('DD/MM/YYYY');
 }
