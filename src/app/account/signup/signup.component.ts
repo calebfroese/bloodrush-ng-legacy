6a5a54d8072f2a386ed24b16fa1d92f4805f7986
Caleb Froese <caleb.froese@gmail.com>
2017-02-07 20:10:28 +1030

    Add newsletter and updates signup to mail chimp campaign

--- src/app/account/signup/signup.component.ts
+++ src/app/account/signup/signup.component.ts
@@ -1,48 +1,72 @@
-import { Component } from '@angular/core';
-import { Router } from '@angular/router';
-import { FormGroup } from '@angular/forms';
+import {Component} from '@angular/core';
+import {FormGroup} from '@angular/forms';
+import {Router} from '@angular/router';
 
-import { environment } from './../../../environments/environment';
-import { FormService } from './../../shared/forms/form.service';
-import { AccountService } from './../../shared/account.service';
-import { CustomValidators } from './../../shared/forms/custom-validators';
+import {environment} from './../../../environments/environment';
+import {AccountService} from './../../shared/account.service';
+import {ApiService} from './../../shared/api/api.service';
+import {CustomValidators} from './../../shared/forms/custom-validators';
+import {FormService} from './../../shared/forms/form.service';
 
-@Component({
-    templateUrl: './signup.component.html'
-})
+@Component({templateUrl: './signup.component.html'})
 export class SignupComponent {
-    form: FormGroup;
-    user: any = {
-        username: '',
-        email: '',
-        password: '',
-        passwordconf: ''
-    };
-    team: any = {
-        acronym: '',
-        name: '',
-    };
+  form: FormGroup;
+  user: any = {username: '', email: '', password: '', passwordconf: ''};
+  team: any = {
+    acronym: '',
+    name: '',
+  };
 
-    constructor(private router: Router, private formService: FormService, private acc: AccountService) {
-        this.form = this.formService.signupForm();
-    }
+  constructor(
+      private router: Router, private formService: FormService,
+      private acc: AccountService, private api: ApiService) {
+    this.form = this.formService.signupForm();
+  }
 
-    onClickSubmit(val: any): void {
-        // Submit
-        this.acc.signup({
-            username: val.username,
-            password: val.password,
-            email: val.email
-        }, {
-                acronym: val.acronym.toUpperCase(),
-                name: val.teamName
-            }).then(() => {
-                if (this.acc.team.verified !== true && environment.envName === 'prod') {
-                    this.acc.logout().then(() => {
-                        alert(`Successfully created an account. Please verify your email address for ${val.teamName} to start playing.`);
-                        this.router.navigate(['/home']);
-                    });
-                }
+  onClickSubmit(val: any): void {
+    // Submit
+    this.acc
+        .signup(
+            {username: val.username, password: val.password, email: val.email},
+            {acronym: val.acronym.toUpperCase(), name: val.teamName})
+        .then(() => {
+          // Add to the mailing lists if applicable
+          if (val.mailUpdates) {
+            this.api
+                .run(
+                    'post', `/emails/addToMailingList`,
+                    `&email=${val.email}&list=updates`, {})
+                .then(res => {
+                  console.log('Signed up for updates');
+                })
+                .catch(err => {
+                  alert(err);
+                  console.error(err);
+                });
+          }
+          if (val.mailNewsletter) {
+            this.api
+                .run(
+                    'post', `/emails/addToMailingList`,
+                    `&email=${val.email}&list=newsletter`, {})
+                .then(res => {
+                  console.log('Signed up for newsletter');
+                })
+                .catch(err => {
+                  alert(err);
+                  console.error(err);
+                });
+          }
+          // Do post-signup stuff
+          if (this.acc.team.verified !== true &&
+              environment.envName === 'prod') {
+            this.acc.logout().then(() => {
+              alert(
+                  `Successfully created an account. Please verify your email address for ${val
+                      .teamName} to start playing.`);
+              this.router.navigate(['/home']);
             });
-    }
+          }
+        });
+  }
 }
--- src/app/shared/forms/form.service.ts
+++ src/app/shared/forms/form.service.ts
@@ -1,92 +1,81 @@
 // Angular imports
-import { Injectable } from '@angular/core';
-import { FormBuilder, Validators, FormGroup } from '@angular/forms';
+import {Injectable} from '@angular/core';
+import {FormBuilder, FormGroup, Validators} from '@angular/forms';
 
+import {CustomValidators} from './custom-validators';
 // Custom imports
-import { USER, TEAM, LEAGUE } from './form.config';
-import { CustomValidators } from './custom-validators';
+import {LEAGUE, TEAM, USER} from './form.config';
 
 @Injectable()
 export class FormService {
-    /**
-     * Used to create forms to be used for markets and traders
-     */
+  /**
+   * Used to create forms to be used for markets and traders
+   */
 
-    // Account
-    email = ['',
-        Validators.compose([
-            Validators.required,
-            CustomValidators.isRaw('isEmail')
-        ])
-    ];
-    password = ['',
-        Validators.compose([
-            Validators.required,
-            Validators.minLength(USER.PASSWORD.MIN)
-        ])
-    ];
-    username = ['',
-        Validators.compose([
-            Validators.required,
-            Validators.minLength(USER.USERNAME.MIN),
-            Validators.maxLength(USER.USERNAME.MAX)
-        ])
-    ];
-    // Team
-    acronym = ['',
-        Validators.compose([
-            Validators.required,
-            Validators.minLength(TEAM.ACRONYM.MIN),
-            Validators.maxLength(TEAM.ACRONYM.MAX)
-        ])
-    ];
-    teamName = ['',
-        Validators.compose([
-            Validators.required,
-            Validators.minLength(TEAM.NAME.MIN),
-            Validators.maxLength(TEAM.NAME.MAX)
-        ])
-    ];
-    // League
-    leagueName = ['',
-        Validators.compose([
-            Validators.required,
-            Validators.minLength(LEAGUE.NAME.MIN),
-            Validators.maxLength(LEAGUE.NAME.MAX)
-        ])
-    ];
-    leaguePublic = [{ value: true, disabled: false }];
+  // Account
+  email = [
+    '',
+    Validators.compose(
+        [Validators.required, CustomValidators.isRaw('isEmail')])
+  ];
+  password = [
+    '',
+    Validators.compose(
+        [Validators.required, Validators.minLength(USER.PASSWORD.MIN)])
+  ];
+  username = [
+    '', Validators.compose([
+      Validators.required, Validators.minLength(USER.USERNAME.MIN),
+      Validators.maxLength(USER.USERNAME.MAX)
+    ])
+  ];
+  checkBox = ['', Validators.compose([])];
+  // Team
+  acronym = [
+    '', Validators.compose([
+      Validators.required, Validators.minLength(TEAM.ACRONYM.MIN),
+      Validators.maxLength(TEAM.ACRONYM.MAX)
+    ])
+  ];
+  teamName = [
+    '', Validators.compose([
+      Validators.required, Validators.minLength(TEAM.NAME.MIN),
+      Validators.maxLength(TEAM.NAME.MAX)
+    ])
+  ];
+  // League
+  leagueName = [
+    '', Validators.compose([
+      Validators.required, Validators.minLength(LEAGUE.NAME.MIN),
+      Validators.maxLength(LEAGUE.NAME.MAX)
+    ])
+  ];
+  leaguePublic = [{value: true, disabled: false}];
 
-    constructor(
-        private fb: FormBuilder
-    ) { }
+  constructor(private fb: FormBuilder) {}
 
-    signupForm(): FormGroup {
-        return this.fb.group(
-            {
-                email: this.email,
-                emailConf: this.email,
-                password: this.password,
-                passwordConf: this.password,
-                username: this.username,
-                acronym: this.acronym,
-                teamName: this.teamName
-            },
-            {
-                validator: Validators.compose([
-                    CustomValidators.fieldsMatch('email', 'emailConf'),
-                    CustomValidators.fieldsMatch('password', 'passwordConf')
-                ])
-            }
-        );
-    }
+  signupForm(): FormGroup {
+    return this.fb.group(
+        {
+          email: this.email,
+          emailConf: this.email,
+          password: this.password,
+          passwordConf: this.password,
+          username: this.username,
+          acronym: this.acronym,
+          teamName: this.teamName,
+          mailUpdates: this.checkBox,
+          mailNewsletter: this.checkBox
+        },
+        {
+          validator: Validators.compose([
+            CustomValidators.fieldsMatch('email', 'emailConf'),
+            CustomValidators.fieldsMatch('password', 'passwordConf')
+          ])
+        });
+  }
 
-    leagueCreate(): FormGroup {
-        return this.fb.group(
-            {
-                name: this.leagueName,
-                public: this.leaguePublic
-            }
-        );
-    }
+  leagueCreate(): FormGroup {
+    return this.fb.group({name: this.leagueName, public: this.leaguePublic});
+  }
 }
