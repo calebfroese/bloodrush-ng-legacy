aa1c4fb847311c791aa3918923d3125084feb2fb
Caleb Froese <caleb.froese@gmail.com>
2017-02-19 11:44:44 +1030

    Fix ladder component

--- src/app/ladder/ladder.component.ts
+++ src/app/ladder/ladder.component.ts
@@ -1,81 +1,88 @@
-import { Component, OnInit, Input } from '@angular/core';
+import {Component, Input, OnInit} from '@angular/core';
 
-import { ApiService } from './../shared/api/api.service';
-import { ScoreService } from './../shared/score.service';
+import {environment} from './../../environments/environment';
+import {ApiService} from './../shared/api/api.service';
+import {Config} from './../shared/config';
+import {ScoreService} from './../shared/score.service';
 
 @Component({
-    selector: 'bloodrush-ladder',
-    templateUrl: './ladder.component.html',
-    styles: [`
+  selector: 'bloodrush-ladder',
+  templateUrl: './ladder.component.html',
+  styles: [`
         .team { cursor: pointer }
     `]
 })
 export class LadderComponent implements OnInit {
-    @Input() league: any;
-    @Input() season: any;
-    teams: any = [];
+  @Input() league: any;
+  @Input() season: any;
+  teams: any = [];
+  config = Config;
+  envName = environment.envName;
 
-    constructor(private scoreService: ScoreService, private api: ApiService) { }
+  constructor(private scoreService: ScoreService, private api: ApiService) {}
 
-    ngOnInit(): void {
-        // Load the teams
-        this.loadTeams();
-    }
+  ngOnInit(): void {
+    // Load the teams
+    this.loadTeams();
+  }
 
-    loadTeams(): void {
-        this.season.teamIds.forEach(teamId => {
-            console.log('found team', teamId)
-            let team = null;
-            this.api.run('get', `/teams/${teamId}`, '', {})
-                .then(t => {
-                    team = t;
-                    // Get the score
-                    console.log('about to query score')
-                    return this.api.run('get', `/teams/score`, `&leagueId=${this.league.id}&seasonId=${this.season.id}&teamId=${teamId}`, {})
-                })
-                .then(response => {
-                    let teamScore = response.score;
-                    console.log('score', teamScore);
-                    let t = {
-                        team: team,
-                        score: teamScore,
-                        ratio: this.scoreService.calculateRatio(teamScore),
-                        pts: this.scoreService.calculatePoints(teamScore)
-                    };
-                    this.teams.push(t);
-                    this.sortByPoints();
-                    return;
-                }).then(() => {});
-        });
-    }
+  loadTeams(): void {
+    this.season.teamIds.forEach(teamId => {
+      let team = null;
+      this.api.run('get', `/teams/${teamId}`, '', {})
+          .then(t => {
+            team = t;
+            // Get the score
+            console.log('about to query score');
+            return this.api.run(
+                'get', `/teams/score`,
+                `&leagueId=${this.league.id}&seasonId=${this.season.id
+                }&teamId=${teamId}`,
+                {})
+          })
+          .then(response => {
+            let teamScore = response.score;
+            console.log('score', teamScore);
+            let t = {
+              team: team,
+              score: teamScore,
+              ratio: this.scoreService.calculateRatio(teamScore),
+              pts: this.scoreService.calculatePoints(teamScore)
+            };
+            this.teams.push(t);
+            this.sortByPoints();
+            return;
+          })
+          .then(() => {});
+    });
+  }
 
-    sortByPoints(): void {
-        // Sorts the array by points
-        let arr = this.teams;
-        arr.sort((a: any, b: any) => {
-            if (b.pts < a.pts) {
-                return -1;
-            } else if (b.pts > a.pts) {
-                return 1;
-            } else {
-                // Sort by least ties
-                if (b.score.t > a.score.t) {
-                    return -1;
-                } else if (b.score.t < a.score.t) {
-                    return 1;
-                } else {
-                    // Sort by least games played
-                    if (b.score.gp > a.score.gp) {
-                        return -1;
-                    } else if (b.score.gp < a.score.gp) {
-                        return 1;
-                    } else {
-                        return 0;
-                    }
-                }
-
-            }
-        });
-        this.teams = arr;
-    }
+  sortByPoints(): void {
+    // Sorts the array by points
+    let arr = this.teams;
+    arr.sort((a: any, b: any) => {
+      if (b.pts < a.pts) {
+        return -1;
+      } else if (b.pts > a.pts) {
+        return 1;
+      } else {
+        // Sort by least ties
+        if (b.score.t > a.score.t) {
+          return -1;
+        } else if (b.score.t < a.score.t) {
+          return 1;
+        } else {
+          // Sort by least games played
+          if (b.score.gp > a.score.gp) {
+            return -1;
+          } else if (b.score.gp < a.score.gp) {
+            return 1;
+          } else {
+            return 0;
+          }
+        }
+      }
+    });
+    this.teams = arr;
+  }
 }
--- src/app/shared/score.service.ts
+++ src/app/shared/score.service.ts
@@ -6,7 +6,7 @@ export class ScoreService {
         if (!teamScore.w) teamScore.w = 0;
         if (!teamScore.t) teamScore.t = 0;
         if (!teamScore.l) return (teamScore.w + (teamScore.t / 2));
-        return teamScore.w + (teamScore.t / 2) / teamScore.l;
+        return (teamScore.w + (teamScore.t / 2) / teamScore.l).toFixed(2);
     }
 
     public calculatePoints(teamScore: any): number {
