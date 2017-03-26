5f6b3846bc003b9d5733a893ac9acd668621186e
Caleb Froese <caleb.froese@gmail.com>
2017-03-26 12:40:07 +1030

    Remove upcoming games until backend fix

--- src/app/upcoming-games/upcoming-games.component.ts
+++ src/app/upcoming-games/upcoming-games.component.ts
@@ -1,12 +1,14 @@
-import { Component, Input, OnInit } from '@angular/core';
-
-import { ApiService } from './../shared/api/api.service';
+import {Component, Input, OnInit} from '@angular/core';
 import * as moment from 'moment';
 
+import {AccountService} from '../shared/account.service';
+
+import {ApiService} from './../shared/api/api.service';
+
 @Component({
-    selector: 'bloodrush-upcoming-games',
-    templateUrl: './upcoming-games.component.html',
-    styles: [`
+  selector: 'bloodrush-upcoming-games',
+  templateUrl: './upcoming-games.component.html',
+  styles: [`
         .card-content {
             height: 150px;
         }
@@ -16,35 +18,38 @@ import * as moment from 'moment';
     `]
 })
 export class UpcomingGamesComponent implements OnInit {
-    @Input() season: any;
-    teams: any = {};
-    public gamesToday: any[] = [];
+  @Input() season: any;
+  teams: any = {};
+  public gamesToday: any[] = [];
 
-    constructor(private api: ApiService) { }
+  constructor(private api: ApiService, public acc: AccountService) {}
 
-    ngOnInit(): void {
-        let doWhile = true;
-        for (let i = 0; i < 31; i++) {
-            this.getGames(moment().add(i, 'days').toDate())
-        }
-        this.loadTeams();
+  ngOnInit(): void {
+    let doWhile = true;
+    for (let i = 0; i < 31; i++) {
+      this.getGames(moment().add(i, 'days').toDate())
     }
+    this.loadTeams();
+  }
 
-    getGames(date): void {
-        let d = JSON.stringify(moment(date).format('YYYY/MM/DD'));
-        this.api.run('get', `/games/allOnDate`, `&date=${d}`, {})
-            .then(res => {
-                let games = res.games;
-                this.gamesToday = this.gamesToday.concat(games);
-            });
-    }
+  getGames(date): void {
+    if (!this.acc.leagues) return;
+    let d = JSON.stringify(moment(date).format('YYYY/MM/DD'));
+    this.api
+        .run(
+            'get', `/games/allOnDate`, `&date=${d}&leagueId=${this.acc.leagues[0].id}`,
+            {})
+        .then(res => {
+          let games = res.games;
+          this.gamesToday = this.gamesToday.concat(games);
+        });
+  }
 
-    loadTeams() {
-        return this.api.run('get', `/teams`, '', {})
-            .then(teams => {
-                teams.forEach(t => {
-                    this.teams[t.id] = t;
-                });
-            });
-    }
+  loadTeams() {
+    return this.api.run('get', `/teams`, '', {}).then(teams => {
+      teams.forEach(t => {
+        this.teams[t.id] = t;
+      });
+    });
+  }
 }
